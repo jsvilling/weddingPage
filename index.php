c41754fd6b3d919461eb62c309c8c7959550427e
Joshua Villing <jsvilling@gmail.com>
2018-03-16 20:49:41 +0100

    add favicon and remove unnecessary comments

--- index.php
+++ index.php
@@ -19,17 +19,13 @@ if(isset($_POST['submit'])){
 ?>
 
 <!DOCTYPE HTML>
-<!--
-	Dimension by HTML5 UP
-	html5up.net | @ajlkn
-	Free for personal and commercial use under the CCA 3.0 license (html5up.net/license)
--->
 <html>
 	<head>
 		<title>Chantal & Joshua</title>
 		<meta charset="utf-8" />
 		<meta name="viewport" content="width=device-width, initial-scale=1, user-scalable=no" />
 		<link rel="stylesheet" href="assets/css/main.css" />
+		<link rel="icon" href="favicon.ico" type="image/x-icon"/>
 		<!--[if lte IE 9]><link rel="stylesheet" href="assets/css/ie9.css" /><![endif]-->
 		<!--noscript><link rel="stylesheet" href="assets/css/noscript.css" /></noscript-->
 	</head>
