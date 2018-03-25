80f050bbfc450aa39955dce672dee3d109d3168c
Joshua Villing <jsvilling@gmail.com>
2018-03-25 18:10:21 +0200

    add basic error handling

--- index.php
+++ index.php
@@ -2,24 +2,26 @@
 // yes, this is php - I'd rather it wasn't but it had to be quick.
 
 if(isset($_POST['submit'])){
-    $to = $_POST['receiver']; 
-    $from = $_POST['email'];
-    $name = $_POST['name'];
-	$answer = $_POST['answer'];
-	$number = $_POST['number'];
-    $subject = "Hochzeit Chantal und Joshua";
-	$message = $_POST['message'];
-    $headers = "From:" . $from;
-
-	if ($answer != null && strlen($answer) > 0) {
-		$message =  " Name: " . $name . " \n\n Antwort: " . $answer . "\n\n Anzahl: " . $number . "\n\n Nachricht: " . $message;
-	} else {
-		$message =  " Name: " . $name . "\n\n Nachricht: " . $message;
+	try {
+		$redirectTarget = 'location: .#thankyou';
+		$to = $_POST['receiver']; 
+		$from = $_POST['email'];
+		$name = $_POST['name'];
+		$answer = $_POST['answer'];
+		$number = $_POST['number'];
+		$subject = "Hochzeit Chantal und Joshua";
+		$message = $_POST['message'];
+		$headers = "From:" . $from;
+		if ($answer != null && strlen($answer) > 0) {
+			$message =  " Name: " . $name . " \n\n Antwort: " . $answer . "\n\n Anzahl: " . $number . "\n\n Nachricht: " . $message;
+		} else {
+			$message =  " Name: " . $name . "\n\n Nachricht: " . $message;
+		}
+		mail($to,$subject,$message,$headers);
+	} catch (Exception $e) {
+		$redirectTarget = 'location: .#opps';
 	}
-	
-	mail($to,$subject,$message,$headers);
-	
-	header('location: .#thankyou');
+	header($redirectTarget);
 }
 ?>
 
