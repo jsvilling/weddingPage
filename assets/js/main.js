62fae5f0fb0b967463d73efb00b01db552a7bf81
Joshua Villing <jsvilling@gmail.com>
2018-04-20 11:48:55 +0200

    fix refresh issue on mobile

--- assets/js/main.js
+++ assets/js/main.js
@@ -223,13 +223,9 @@
 			
 		// Initial article.
 		if (location.hash != '' &&	location.hash != '#') {
-			$window.on('load', function() {
-				$main._load();
-				$main._show(location.hash.substr(1), true);
-			});
+			$main._load();
+			$main._show(location.hash.substr(1), true);
 		} else {
-			console.log("elsa");
-			// Hide main
 			$main.hide();
 		}
 	});
